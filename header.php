3fe0e51280b6e51a39f869c9a134bc1b270ffa39
BuildTools <scotth0828@gmail.com>
2020-11-25 00:18:33 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>ComplexRP</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+		<script src="https://kit.fontawesome.com/d756906828.js" crossorigin="anonymous"></script>
+
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	</head>
+	<body>
+	<div class="container-fluid col-md-12" style="background-color: #e8e8e8;">
+	<?php include 'nav.php'; ?>
